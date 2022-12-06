4ebb1b5a2f7ee03147566302a01c26d07732328d
Gavin Veitch <gavintveitch@gmail.com>
2022-12-06 13:28:05 -0700

    added ruby routes

--- app/app/models/project.rb
+++ app/app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+
+    has_many :comments
+    has_many :users, through: :comments
+
+    validates :date, :time, :progress, :challenge, :bug, :image, presence: true
+
 end
--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+    has_many :comments
+    has_many :projects, through: :comments
+
+    validates :username, :password, :display_name, presence: true
+
 end
